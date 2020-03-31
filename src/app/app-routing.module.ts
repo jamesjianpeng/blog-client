1f72fd122615995c32dbb9414f5d019be0ea74f7
jamesjianpeng <jamesjinapeng@gmail.com>
2020-04-01 07:47:06 +0800

    featrue: add version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-// import { VersionListComponent } from './modules/version/version-list-page/version-list.component'
+import { VersionListComponent } from './modules/version/version-list-page/version-list.component'
 const routes: Routes = [
-  // { path: 'version', component: VersionListComponent },
+  { path: 'version', component: VersionListComponent },
   { path: 'article', loadChildren: () => import('src/app/modules/article/article.module').then(m => m.ArticleModule) },
   { path: '', component: HomeComponent },
 ];
